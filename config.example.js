9f7948cd5a2832af81dde1e3e9905e5cfbd7f724
Sami Kabbaj <kabbajsami2006@gmail.com>
2026-06-04 23:02:14 +0200

    docs: add config example

---
+++ config.example.js
@@ -0,0 +1,6 @@
+const QUIZ_CONFIG = {
+  title: "COURSE-CODE",
+  subtitle: "Course full name",
+  engineCdn: "https://cdn.jsdelivr.net/gh/sami-epfl/ba4-quiz-engine@main",
+  engineLocal: "../ba4-quiz-engine",
+};
