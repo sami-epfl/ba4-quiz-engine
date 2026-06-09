2e64b526bcf4780c568d074b539e07afaec4b391
Sami Kabbaj <kabbajsami2006@gmail.com>
2026-06-10 00:05:57 +0200

    docs: update config template

--- config.example.js
+++ config.example.js
@@ -3,4 +3,6 @@ const QUIZ_CONFIG = {
   subtitle: "Course full name",
   engineCdn: "https://cdn.jsdelivr.net/gh/sami-epfl/ba4-quiz-engine@main",
   engineLocal: "../ba4-quiz-engine",
+  landingUrl: "https://sami-epfl.github.io/ba4-quizzes/",
+  repoUrl: "https://github.com/..",
 };
